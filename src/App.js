e41c59e3a34301c6b11fde261ef960f7a9f4a196
kobars <kobar.sept@gmail.com>
2021-07-01 21:02:26 +0700

    add show / hide button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Selection from "./Components/Selection";
 import Label from "./Components/Label";
 import { emotionOptions, reactions } from "./Utils";
 
-function App() {
+const Form = () => {
   const [emotion, setEmotion] = useState(emotionOptions[0].value);
   const handleChange = (e) => {
     if (e.target) {
@@ -12,22 +12,37 @@ function App() {
     }
   };
   return (
+    <form>
+      <Label labelID="emotion" content="Select emotions" />
+      <Selection
+        selectionID="emotion"
+        options={emotionOptions}
+        value={emotion}
+        onSelectionChange={handleChange}
+      />
+      <Label labelID="reaction" content="Reaction" />
+      <Input
+        // key={emotion}
+        inputID="reaction"
+        defaultValue={reactions[emotion]}
+      />
+    </form>
+  );
+};
+
+function App() {
+  const [isShow, setShow] = useState(true);
+  const handleButtonClick = () => {
+    setShow(!isShow);
+    // sama dengan bawah
+    // setShow((prev) => !prev);
+  };
+  return (
     <div className="container pt-2">
-      <form>
-        <Label labelID="emotion" content="Select emotions" />
-        <Selection
-          selectionID="emotion"
-          options={emotionOptions}
-          value={emotion}
-          onSelectionChange={handleChange}
-        />
-        <Label labelID="reaction" content="Reaction" />
-        <Input
-          // key={emotion}
-          inputID="reaction"
-          defaultValue={reactions[emotion]}
-        />
-      </form>
+      {isShow && <Form />}
+      <button onClick={handleButtonClick} className="btn btn-primary mt-4">
+        Show / Hide
+      </button>
     </div>
   );
 }
